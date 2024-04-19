b90a8783133b155943f392df8fe8489abd1086dc
lukewain <lukewainhouse@outlook.com>
2024-04-19 21:23:26 +0100

    Update sign in and added sign up

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,10 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
     <main>
       <div className="flex w-full h-[80vh] items-center justify-center">
-        <SignIn />
+        <SignUp />
       </div>
     </main>
   );
