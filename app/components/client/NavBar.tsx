c7035d88ce4ea3fc289afdf19377e3209e5faae2
lukewain <lukewainhouse@outlook.com>
2024-04-24 08:21:29 +0100

    Added current time to navbar

--- app/components/client/NavBar.tsx
+++ app/components/client/NavBar.tsx
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 import ThemeToggle from "./ThemeToggle";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import MapLinks from "./MapButtons";
+import { TimeStatus } from "../ui/CurrentTime";
 
 function NavBar() {
   const cookieStore = cookies();
@@ -10,6 +11,9 @@ function NavBar() {
     <nav className="flex items-center flex-wrap p-3" id="navbar">
       <span className="text-xl font-extrabold tracking-wide ml-3">
         Westleigh Inventory System
+      </span>
+      <span className="ml-3 font-bold" id="current-time">
+        <TimeStatus />
       </span>
       <div className="w-auto justify-end inline-flex flex-grow mr-3 gap-5">
         <MapLinks />
